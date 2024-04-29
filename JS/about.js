d2e1d672f4b5b82652099a2a4ba3dbd7489a1d2a
KelioXenelly <157220950+KelioXenelly@users.noreply.github.com>
2024-04-29 12:46:52 +0700

    Add files via upload

---
+++ JS/about.js
@@ -0,0 +1,9 @@
+// Download CV Start
+
+let cv = document.getElementById("downloadCV");
+
+cv.onclick = () => {
+  window.location.href="https://kelioxenelly.github.io/UTS-KelioXenelly-23110001-STI/assets/Curriculum Vitae - Kelio Xenelly.pdf"
+};
+
+// Download CV End
--- JS/script.js
+++ JS/script.js
@@ -13,13 +13,3 @@ window.onscroll = () => {
 };
 
 // Navbar End
-
-// Download CV Start
-
-let cv = document.getElementById("downloadCV");
-
-cv.onclick = () => {
-  window.location.href="https://kelioxenelly.github.io/UTS-KelioXenelly-23110001-STI/assets/Curriculum Vitae - Kelio Xenelly.pdf"
-};
-
-// Download CV End
